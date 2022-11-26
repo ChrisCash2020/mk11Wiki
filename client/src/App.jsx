30446cefb12c321c98a15cc8a4bdbda047cf96b4
chris <chrisolo2001@gmail.com>
2022-11-25 21:50:16 -0600

    Added login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,30 +2,36 @@ import { AuthContext } from './helpers/AuthContext';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Character from './pages/Character';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   return (
-    <Router>
-      <div className='navbar'>
-        <div className='links'>
-          <Link to='/'>Mortal Kombat 11 Wikipedia</Link>
-          {false ? (
-            <div className='loggedInContainer'>
-              <h1>User</h1>
-              <button> Logout</button>
-            </div>
-          ) : (
-            <div className='loggedInContainer'>
-              <Link to='/login'>Login</Link>{' '}
-              <Link to='/register'>Register</Link>
-            </div>
-          )}
+    <AuthContext.Provider>
+      <Router>
+        <div className='navbar'>
+          <div className='links'>
+            <Link to='/'>Mortal Kombat 11 Wikipedia</Link>
+            {false ? (
+              <div className='loggedInContainer'>
+                <h1>User</h1>
+                <button> Logout</button>
+              </div>
+            ) : (
+              <div className='loggedInContainer'>
+                <Link to='/login'>Login</Link>
+                <Link to='/register'>Register</Link>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <Routes>
-        <Route path='/character:id' element={<Character />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path='/character:id' element={<Character />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+export default function Login() {
+  return (
+    <div className='container'>
+      <div className='loginContainer'>
+        <label>Username:</label>
+        <input type='text' />
+        <label>Password:</label>
+        <input type='password' />
+        <button> Login </button>
+      </div>
+    </div>
+  );
+}
